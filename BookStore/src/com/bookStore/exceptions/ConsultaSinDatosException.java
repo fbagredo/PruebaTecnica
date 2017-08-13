8b8f388d0b5e423a5039b38619d813dda3a04f48
Fernando Becerra <fernando.becerra.a@gmail.com>
2017-08-13 06:45:33 +0100

    commit

---
+++ BookStore/src/com/bookStore/exceptions/ConsultaSinDatosException.java
@@ -0,0 +1,14 @@
+package com.bookStore.exceptions;
+
+public class ConsultaSinDatosException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1946238996618507483L;
+
+	public ConsultaSinDatosException(){
+		super("La consulta no arrojo resultados.");
+	}
+
+}
